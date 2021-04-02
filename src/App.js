c166c683b4272d64f1be8004b8955421d8b49933
Michael Staton <mpstaton@gmail.com>
2021-04-02 09:57:54 -0700

    2.25 moved ToDo card to a component file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ function App() {
   return (
     <div>
       <h1>To Do List</h1>
-        <div className='card'>
-          <h2>TITLE</h2>
-          <div className='actions'>
-            <button className='btn'>Delete</button>
-          </div>
-        </div>
+        
     </div>
   );
 }
