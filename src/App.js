1eb1c9fec93b31b7f44b90e8606174b5bff74df5
Michael Staton <mpstaton@gmail.com>
2021-04-02 09:42:30 -0700

    2.24 styling component with CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1>To Do List</h1>
+        <div className='card'>
+          <h2>TITLE</h2>
+          <div className='actions'>
+            <button className='btn'>Delete</button>
+          </div>
+        </div>
     </div>
   );
 }
