615c1f61984684011759e09a909383118d207764
Michael Staton <mpstaton@gmail.com>
2021-04-03 15:51:21 -0700

    3.33 scoping styles with CSS modules

--- src/components/layout/MainNaviation.js
+++ src/components/layout/MainNaviation.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 
+//this uses a React library called CSS modules
+import classes from './MainNavigation.module.css';
+
 function MainNavigation() {
-    return <header>
-        <div>React Meetups</div>
+    return <header className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
         <nav>
             <ul>
                 <li>
