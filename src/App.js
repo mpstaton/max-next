535e5afc39a22118654eff5e47fb9c5c5bcf43f1
Michael Staton <mpstaton@gmail.com>
2021-04-03 09:55:22 -0700

    restarting codebase at 2.31 adding routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>To Do List</h1>
-        <Todo text='Wim Hof Breath'/>
-        <Todo text='Breathless Pushups'/>
-        <Todo text='Meditate'/>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
