82c2a4d1f5753d6db58b7dce02aafdd3b6643787
Michael Staton <mpstaton@gmail.com>
2021-04-04 13:20:02 -0700

    2.43 useEffect working code not sure why it works

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,58 +1,50 @@
-//useEffect is a hook that allows you to run code under certain conditions.
 import { useState, useEffect } from 'react';
 
 import MeetupList from '../components/meetups/MeetupList';
 
 function AllMeetupsPage() {
-  /* you cana have as many "states" as you want or need */
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  /* useEffect hook gets two arguments: a function and an array of dependencies */  
   useEffect(() => {
     setIsLoading(true);
     fetch(
       'https://max-next-default-rtdb.firebaseio.com/meetups.json'
     )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      /* because we use the map method the data needs to be converted from
-      an object to an array */ 
-      const meetups = [];
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
 
-      for (const key in data) {
-        const meetup = {
-          id: key,
-          ...data[key]
-        };
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
 
-        meetups.push(meetup);
+          meetups.push(meetup);
+        }
 
-      }
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
   }, []);
 
-  /* functions must immediately return JSX*/
-
-
-    if (isLoading) {
-      return (
-        <section>
-          <p>Loading...</p>
-        </section>
-      )
-    }
-
+  if (isLoading) {
     return (
-    <section>
-        <h1>All Meetups</h1>
-        <MeetupList meetups={loadedMeetups} />
-    </section>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
