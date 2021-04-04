48c38f574522c3b2ede65b6d0da8925f5dbb016a
Michael Staton <mpstaton@gmail.com>
2021-04-04 07:04:32 -0700

    2.39 sending data through POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,24 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 
 function NewMeetupPage() {
+    /*firebase automagically creates the meetups collection if it doesn't exist.
+    the fetch function is built into JavaScript and defaults to GET. */
+    function addMeetupHandler(meetupData) {
+        fetch('https://max-next-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        );
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm/>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
